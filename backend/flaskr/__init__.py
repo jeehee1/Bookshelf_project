8e56a65cc8e754cd14c2b713f4fa54bb0fe1d91e
JiheeW <jeehee1w@gmail.com>
2021-12-30 02:26:54 +0900

    create book method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -48,7 +48,7 @@ def create_app(test_config=None):
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
 
-    @app.route('/books')
+    @app.route('/books', methods=['GET'])
     def get_books():
         selection = Book.query.order_by(Book.id).all()
         current_books = paginate_book(request, selection)     
@@ -131,5 +131,28 @@ def create_app(test_config=None):
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+
+    @app.route('/books', methods=['POST'])
+    def create_book():
+        body = request.get_json()
+        new_title = body.get('title', None)
+        new_author = body.get('author', None)
+        new_rating = body.get('rating', None)
+        try:
+            book = Book(title=new_title, author=new_author, rating=new_rating)
+            book.insert()
+            
+            selection = Book.query.order_by(Book.id).all()
+            current_books = paginate_book(request, selection)
+
+            return jsonify({
+                'success' : True,
+                'created_id' : book.id,
+                'books' : current_books,
+                'total_books' : len(selection)
+            })
+
+        except:
+            abort(422)
 
     return app
